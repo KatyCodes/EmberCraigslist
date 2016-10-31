f682d3a598bacbf0caf74129ccbaa6e23f9c60ce
Katy Henning and Zachary Matthewstearn <zachary@>
2016-10-31 13:38:49 -0700

    clear new post form on submission and fix cancel new post button and require user input for all fields

--- app/components/post-add.js
+++ app/components/post-add.js
@@ -19,8 +19,20 @@ export default Ember.Component.extend({
         location: this.get('location'),
         date: this.get('date'),
       };
-      this.set('addPostOpen', false);
-      this.sendAction('saveNewPost2', params);
+      if(params.author && params.content && params.title && params.image && params.price && params.location && params.date) {
+        this.set('addPostOpen', false);
+        this.sendAction('saveNewPost2', params);
+      }
+      else {
+        alert("All fields are required to submit a new post.");
+      }
+      this.set('author', "");
+      this.set('content', "");
+      this.set('title', "");
+      this.set('image', "");
+      this.set('price', "");
+      this.set('location', "");
+      this.set('date', "");
     }
   }
 });
