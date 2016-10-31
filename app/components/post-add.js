ff31d448792c76a47d05ae655e648cb2c67adce1
Katy Henning and Zachary Matthewstearn <zachary@>
2016-10-31 13:31:37 -0700

    complete functaionality to add new posts

--- app/components/post-add.js
+++ app/components/post-add.js
@@ -8,6 +8,19 @@ export default Ember.Component.extend({
     },
     closePostAdd() {
       this.set('addPostOpen', false);
+    },
+    saveNewPost1 () {
+      var params ={
+        author: this.get('author'),
+        content: this.get('content'),
+        title: this.get('title'),
+        image: this.get('image'),
+        price: parseInt(this.get('price')),
+        location: this.get('location'),
+        date: this.get('date'),
+      };
+      this.set('addPostOpen', false);
+      this.sendAction('saveNewPost2', params);
     }
   }
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -3,5 +3,11 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('post');
+  },
+  actions: {
+    saveNewPost3(params) {
+      var newPost = this.store.createRecord('post', params);
+      newPost.save();
+    }
   }
 });
